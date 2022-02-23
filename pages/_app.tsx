d34341afde60d2bd679bbea76975e5d6e7b06159
Ehsan Sadeghi <sadeghii.ehsan@gmail.com>
2022-02-23 13:09:23 +0330

    todo app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import "@fontsource/fredoka-one"; // Defaults to weight 400.
+import '../styles/Todo.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
